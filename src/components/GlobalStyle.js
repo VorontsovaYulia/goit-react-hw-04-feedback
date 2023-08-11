88908ba68fe6928d5eb4a2b3c01a9fdcac7e7367
VorontsovaYulia <vorontsovayulia89@gmail.com>
2023-08-11 08:55:49 +0300

    add css

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -3,16 +3,34 @@ import 'modern-normalize';
 
 export const GlobalStyle = createGlobalStyle`
 body {
-  margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    width: 400px;
+    margin: 0 auto;
+    padding: 15px;
+    text-align: center;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
     sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+}
+
+button {
+    margin: 0 10px;
+    display: inline-block;
+}
+
+button:hover{
+    background-color: #999;
+    color: #ffcc00;
+}
+
+:first-letter{
+    text-transform: uppercase;
 }
 
 code {
-  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
     monospace;
 }
 `
